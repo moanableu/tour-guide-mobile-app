4ae60be7b39ccd346652e8919831c97940de6916
Anahi Costa <moanableu@gmail.com>
2018-07-04 11:23:49 -0400

    nav functionality tested, card layout fixed icon to right

---
+++ app/src/main/java/com/example/android/tourguide/SeightseeingFragment.java
@@ -0,0 +1,33 @@
+package com.example.android.tourguide;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class SeightseeingFragment extends Fragment {
+
+    public SeightseeingFragment(){
+        // required emtpy constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.rb_cardview,container,false);
+
+        final ArrayList<Location> locations = new ArrayList <Location>();
+        locations.add(new Location(R.drawable.seightseeing, "Museumsinsel", "One place for all the arts", "shop.smb.museum", "10-6pm", "Berlin", "Free"));
+        locations.add(new Location(R.drawable.seightseeing,"Museumsinsel1", "One place for all the arts", "shop.smb.museum", "10-6pm", "Berlin", "Free"));
+        locations.add(new Location(R.drawable.seightseeing,"Museumsinsel2", "One place for all the arts", "shop.smb.museum", "10-6pm", "Berlin",  "Free"));
+
+        return rootView;
+    }
+}
